90f06931e87feedbd79457bd2c60559b262ed1f7
DanielFAdarve <danielfelipeavalencia@gmail.com>
2025-08-05 22:26:39 -0500

    Add. Se integra cors para uso del front

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 //Importar Express
 const express = require('express');
+const cors = require('cors');
 
 // Carga las rutas del Proyecto
 const patient = require('./routes/patient.route');
@@ -9,9 +10,29 @@ const cookieParser = require('cookie-parser');
 const auth = require('./routes/auth.route');
 const app = express();
 
+
+const allowedOrigins = [
+    'http://localhost:4200',
+  ];
+  
+app.use(cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('No permitido por CORS'));
+      }
+    },
+    credentials: true
+  }));
+
+
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(errorHandler);
+
+
 
 //Cargar las rutas
 app.use('/auth', auth);
