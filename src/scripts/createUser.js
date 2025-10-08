ff487b6db995d6634d268e8c587eac18602e4120
DanielFAdarve <108020493+DanielFAdarve@users.noreply.github.com>
2025-10-07 23:52:02 -0500

    update create user

--- src/scripts/createUser.js
+++ src/scripts/createUser.js
@@ -1,5 +1,7 @@
 const { sequelize, User } = require('../models');
 
+/* This code block is using an asynchronous function in JavaScript to interact with a database using
+Sequelize ORM. Here's a breakdown of what it's doing: */
 (async () => {
   await sequelize.sync();
   const user = await User.create({
